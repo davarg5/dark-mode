798a0c835b4229dabcb95f708ec921e16309110f
Daniel Vargas <davarg5@yahoo.com>
2020-10-28 16:38:11 -0400

    finished useDarkMode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,5 +2,11 @@ import { useState } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = key => {
+    const [someValue, setSomeValue] = useLocalStorage(key, false);
     
+    const toggleDarkMode = () => {
+        setSomeValue(!someValue);
+    }
+
+    return [someValue, toggleDarkMode];
 }
